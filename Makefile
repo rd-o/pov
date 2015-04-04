b271717467802077b2aa203d5f18ba1ac5c6f644
rd-o <rd.qnd.m@gmail.com>
2015-04-04 09:19:11 -0400

    Now it is possible to show some characters on the screen

--- Makefile
+++ Makefile
@@ -1,10 +1,12 @@
-all: pov.hex
+name=pov
 
-pov.hex: pov.elf 
+all: $(name).hex
+
+$(name).hex: $(name).elf 
 	avr-objcopy -j .text -j .data -O ihex $^ $@
 
-pov.elf: pov.c
+$(name).elf: $(name).c
 	avr-gcc -mmcu=atmega328 -Wall -Os -o $@ $^
 
-up: pov.hex
+up: $(name).hex
 	sudo avrdude -p m328 -c usbtiny -e -U flash:w:$^
--- pov.c
+++ pov.c
@@ -1,33 +1,44 @@
 #define F_CPU 1000000UL
+#define __DELAY_BACKWARD_COMPATIBLE__
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <inttypes.h>
 
+//a 0b11111000, 0b10010000, 0b10010000, 0b11111000
+//i 0b10001000, 0b11111000, 0b10001000
+//n 0b11111000, 0b01000000, 0b00100000, 0b11111000
+//m 0b11111000, 0b01000000, 0b00100000, 0b01000000, 0b11111000
+//l 0b11111000, 0b00001000, 0b00001000, 0b00001000
+
+
+uint8_t caracteres[] = {0b11111000, 0b10010000, 0b10010000, 0b11111000, 0b00000000, 0b11111000, 0b01000000, 0b00100000, 0b11111000, 0b00000000, 0b10001000, 0b11111000, 0b10001000, 0b00000000, 0b11111000, 0b01000000, 0b00100000, 0b01000000, 0b11111000, 0b00000000, 0b11111000, 0b10010000, 0b10010000, 0b11111000, 0b00000000, 0b11111000, 0b00001000, 0b00001000, 0b00001000};
+
+//uint8_t caracteres[] = {0b11111000, 0b10101000, 0b10101000, 0b10001000};
+uint8_t retardo_linea_v;
+
 int main(void){
-    DDRD &= ~(1 << DDD2); //poner a 0 PD2 para que sirva de entrada
+    retardo_linea_v = 2;
 
-    PORTD |= (1 << PORTD2);  //activar pull-up en PD2
-
+    DDRD &= ~(1 << DDD2);     //poner a 0 PD2 para que sirva de entrada
+    PORTD |= (1 << PORTD2);   //activar pull-up en PD2
     EICRA |= (1 << ISC00);    // INTO sera activado en cualquier cambio logico
     EIMSK |= (1 << INT0);     // activar INT0
-
     sei();                    // activar interrupciones
 
     DDRB |= _BV(DDB0); //PB0 como salida
-    
     while(1)
     {
-        //PORTB ^= _BV(PB0); //cambiar estado de PB0
-        //_delay_ms(500); //retardo de 500ms
     }
 }
 
-
-
 ISR (INT0_vect)
 {
-    PORTB ^= _BV(PB0); //cambiar estado de PB0
-    _delay_ms(500); //retardo de 500ms
+    int i;
+    for(i = 0; i < 15; i++){
+        PORTB = caracteres[i]; 
+        _delay_ms(retardo_linea_v); //retardo
+    }
+    PORTB = 0;
 }
